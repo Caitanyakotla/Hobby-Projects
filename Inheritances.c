e287c08242a9aaff801f768f5b51fc84bbd37486
sai krishna chaitanya kotla <43471401+Caitanyakotla@users.noreply.github.com>
2018-09-24 03:04:11 +0200

    Add files via upload

---
+++ Inheritances.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,width,height;
+    scanf("%d%d",&width,&height);
+    printf("%d %d",width,height);
+    a=width*height;
+    printf("\n%d",a);
+	
+}
---
+++ List.py
@@ -0,0 +1,12 @@
+'''
+Subdomain : Numpy
+Domain    : Python
+'''
+import numpy
+n = int(input())
+ar = []
+for i in range(n):
+    tmp = list(map(float,input().split()))
+    ar.append(tmp)
+np_ar = numpy.array(ar,float)
+print(numpy.linalg.det(np_ar))
